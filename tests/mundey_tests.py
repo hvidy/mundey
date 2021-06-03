47384e2c6062c7fea43e91f1c58c351913d24e25
Benjamin Pope <benjaminpope@192-168-1-104.tpgi.com.au>
2021-06-03 17:07:14 +1000

    tests

--- tests/mundey_tests.py
+++ tests/mundey_tests.py
@@ -3,11 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import mundey
+from pathlib import Path
+ddir = str(Path(__file__).parent.parent.resolve() / 'notebooks/')+'/'
 
 import pytest
 
+
 def test_sirius():
-	ddir = '../notebooks/'
 
 	tpffile = ddir+'tess2018349182459-s0006-0000000322899250-0126-s_tp.fits'
 
